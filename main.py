91e269915c6bbc171fdf4c01ca17aa114d4d157f
doubledippinz <47015605+doubledippinz@users.noreply.github.com>
2021-12-14 17:04:10 +0000

    Further Tidying up and added more function

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-import datetime
-
-currentyear = datetime.date.today().year
-year3 = (datetime.date.today().year - 3)
-year2 = datetime.date.today().year - 2
-year1 = datetime.date.today().year - 1
+# import datetime
+# currentyear = datetime.date.today().year
+# year3 = (datetime.date.today().year - 3)
+# year2 = datetime.date.today().year - 2
+# year1 = datetime.date.today().year - 1
 aathreshold = 240000
 aallowance = 40000
 maxaareduction = 36000
 
 
 class Client:
-
     def __init__(self, name, salary, bonus, intdiv, employeepercent, employerpercent, pensionlump, salaryexchange,
-                 deathbenefits):
+                 deathbenefits, year3contribution, year2contribution, year1contribution):
         self.name = name
         self.salary = salary
         self.bonus = bonus
@@ -23,23 +21,28 @@ class Client:
         self.salaryexchange = salaryexchange
         self.deathbenefits = deathbenefits
         self.totalgross = salary + bonus + intdiv
+        self.totalannualpensioncontributions = self.employeecontribution + self.employercontribution + self.pensionlump
+        self.year3contribution = year3contribution
+        self.year2contribution = year2contribution
+        self.year1contribution = year1contribution
 
     @classmethod
     def from_input(cls):
         return cls(input('Name: '),
                    int(input('Salary + Bonus: ')),
-                   int(input('Bonus & Comission')),
+                   int(input('Bonus & Comission: ')),
                    int(input('Interest & Dividends: ')),
                    int(input('Employee gross workplace contribution % : ')),
                    int(input('Employer gross workplace contribution % :')),
                    int(input('Lump sum contributions: ')),
                    int(input('Employment income given through salary exchange')),
-                   int(input('Taxed lump sum death benefits: '))
+                   int(input('Taxed lump sum death benefits: ')),
+                   int(input('Please enter your pension contributions for 2018: ')),
+                   int(input('Please enter your pension contributions for 2019: ')),
+                   int(input('Please enter your pension contributions for 2020: '))
                    )
 
-    @staticmethod
-    def get_carryforward():
-
+    def get_carryforward(self):
         def cap_carryforward(contribution):
             if contribution > aallowance:
                 contribution = aallowance
@@ -49,40 +52,56 @@ class Client:
                 remaining_allowance = aallowance - contribution
                 return remaining_allowance
 
-        print("Please enter your pension contributions for ", year3)
-        year3contribution = int(input())
-        year3remallowance = cap_carryforward(year3contribution)
-        print("Please enter your pension contributions for ", year2)
-        year2contribution = int(input())
-        year2remallowance = cap_carryforward(year2contribution)
-        print("Please enter your pension contributions for ", year1)
-        year1contribution = int(input())
-        year1remallowance = cap_carryforward(year1contribution)
+        year3remallowance = cap_carryforward(self.year3contribution)
+        year2remallowance = cap_carryforward(self.year2contribution)
+        year1remallowance = cap_carryforward(self.year1contribution)
         cf_allowance = year3remallowance + year2remallowance + year1remallowance
         return cf_allowance
 
     def get_threshold(self):
-        threshinc = self.totalgross - self.pensionlump - self.employeecontribution + self.salaryexchange - self.deathbenefits
+        threshinc = self.totalgross - self.pensionlump - self.employeecontribution\
+                    + self.salaryexchange - self.deathbenefits
         return threshinc
 
     def get_adjustedincome(self):
         adjustedinc = self.totalgross + self.employercontribution - self.deathbenefits
         return adjustedinc
 
-    def get_annualallowance(self):
+    def get_remaining_aa(self):
         taperexcess = 0
         if Client.get_adjustedincome(self) > aathreshold:
             taperexcess = (Client.get_adjustedincome(self) - aathreshold) / 2
             if taperexcess > maxaareduction:
                 taperexcess = maxaareduction
         annualallowance = aallowance - taperexcess
-        return annualallowance
+        remaining_aa = annualallowance - self.totalannualpensioncontributions
+        if remaining_aa <= 0:
+            remaining_aa = 0
+        return remaining_aa
+
+    def get_maximum_contribution(self):
+        combinedallowance = (Client.get_remaining_aa(self) + Client.get_carryforward(self))
+        if (self.salary - combinedallowance) <= 0:
+            maxcontribution = self.salary
+            return maxcontribution
+        elif (self.salary - combinedallowance) > 0:
+            maxcontribution = combinedallowance
+            return maxcontribution
 
 
-c = Client.from_input()
 
-# print(c.get_threshold())
-# print(c.get_adjustedincome())
-# print(c.get_annualallowance())
+client1 = Client.from_input()
+print(client1.name, 'Maximum pension contribution you can make this tax year: ', client1.get_maximum_contribution())
+print(client1.name, 'Maximum pension contribution your employer can make this year: ', client1.get_carryforward() + client1.get_remaining_aa())
+print(client1.name, 'Remaining annual allowance: ', client1.get_remaining_aa())
+print(client1.name, 'Carryforward available: ', client1.get_carryforward())
 
-print(c.get_carryforward())
+client2 = Client.from_input()
+
+print(client2.name, 'Maximum pension contribution you can make this tax year: ', client2.get_maximum_contribution())
+print(client2.name, 'Maximum pension contribution your employer can make this year: ', client2.get_carryforward() + client1.get_remaining_aa())
+print(client2.name, 'Remaining annual allowance: ', client2.get_remaining_aa())
+print(client2.name, 'Carryforward available: ', client2.get_carryforward())
+
+
+
